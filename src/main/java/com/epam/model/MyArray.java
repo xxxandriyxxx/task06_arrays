16cc969a0c5356f13038bced609399ea368d4a22
Andriy <andriymattt@gmail.com>
2019-11-24 12:05:20 +0200

    array generation method changed to static

--- src/main/java/com/epam/model/MyArray.java
+++ src/main/java/com/epam/model/MyArray.java
@@ -1,13 +1,12 @@
 package com.epam.model;
 
 public class MyArray {
-    private int[] array;
-    private int max = 10;
+    private static int max = 10;
 
-    public int[] MyArray(int size) {
-        array = new int[size];
+    static int[] getArray(int size) {
+        int[] array = new int[size];
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * ++max);
+            array[i] = (int) (Math.random() * (max + 1));
         }
         return array;
     }
