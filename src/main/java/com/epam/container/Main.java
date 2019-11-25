b7834333b55eec624062889c9edbc9f66633364a
Andriy <andriymattt@gmail.com>
2019-11-25 22:09:50 +0200

    add performance comparison of adding 10000 elements to Main class

--- src/main/java/com/epam/container/Main.java
+++ src/main/java/com/epam/container/Main.java
@@ -25,6 +25,26 @@ public class Main {
         container.remove("fff");
         System.out.println("--------------------------- remove('fff')");
         System.out.println(container.toString());
-        
+
+        Timer timer = new Timer();
+        ArrayBusinessLogic arrayBusinessLogic = new ArrayBusinessLogic();
+        int[] intArray = arrayBusinessLogic.generateArray(10000);
+        StringContainer<String> container2 = new StringContainer<>();
+        List<String> list = new ArrayList<>();
+        System.out.println("=================== performance comparison of adding 10000 elements");
+
+        timer.start();
+        for (int i = 0; i < intArray.length; i++) {
+            container2.add(Integer.toString(intArray[i]));
+        }
+        timer.stop();
+        System.out.println("Container : " + timer);
+
+        timer.start();
+        for (int i = 0; i < intArray.length; i++) {
+            list.add(Integer.toString(intArray[i]));
+        }
+        timer.stop();
+        System.out.println("ArrayList : " + timer);
     }
 }
