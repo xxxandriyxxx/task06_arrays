f91fa938306d9de74adabd0f3b6731624ba2dc0e
Andriy <andriymattt@gmail.com>
2019-11-26 21:39:01 +0200

    add getters and setters to Country class

--- src/main/java/com/epam/comparator/Country.java
+++ src/main/java/com/epam/comparator/Country.java
@@ -2,11 +2,27 @@ package com.epam.comparator;
 
 public class Country implements Comparable<Country> {
 
-    protected String name;
-    protected String capital;
+    private String name;
+    private String capital;
 
     public Country(String name, String capital) {
         this.name = name;
+        this.capital = capital;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
         this.capital = capital;
     }
 
